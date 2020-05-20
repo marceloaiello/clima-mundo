bd4e8534eaede74b68474f3cfe7da5ef1d33da5d
Marcelo Aiello <marcelo.aiello@gmail.com>
2020-05-20 16:05:47 -0300

    correccion de fix

--- clima/clima.js
+++ clima/clima.js
@@ -6,7 +6,7 @@ const getClima = async(lat, lon) => {
     //const instance = axios.create({
     //    baseURL: `api.openweathermap.org/data/2.5/weather?lat= ${ lat } &lon=${ lon }&appid=903c7b6cc4bedcf4439f6c149c5c005a&units=metric`
     //});
-    //console.log(`api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=903c7b6cc4bedcf4439f6c149c5c005a&units=metric`);
+    //console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=903c7b6cc4bedcf4439f6c149c5c005a&units=metric`);
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=903c7b6cc4bedcf4439f6c149c5c005a&units=metric`);
     //const resp = await instance.get();
 
